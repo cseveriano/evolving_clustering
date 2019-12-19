5673c4eba3e43f4e8afdfd2cbc5b2d771d2f67ea
Carlos Severiano <carlossjr@gmail.com>
2019-12-19 18:28:42 -0300

    Another trial for norm

--- src/models/evolving/EvolvingClustering.py
+++ src/models/evolving/EvolvingClustering.py
@@ -70,7 +70,8 @@ class EvolvingClustering:
     @staticmethod
     @jit(nopython=True)
     def update_variance(delta, s_ik, var_ik):
-        variance = ((s_ik - 1) / s_ik) * var_ik + (((np.linalg.norm(delta) * (1 / math.sqrt(len(delta))))** 2 / (s_ik - 1)))
+#        variance = ((s_ik - 1) / s_ik) * var_ik + (((np.linalg.norm(delta) * (1 / math.sqrt(len(delta))))** 2 / (s_ik - 1)))
+        variance = ((s_ik - 1) / s_ik) * var_ik + ((np.linalg.norm(delta) ** 2) / (s_ik - 1))
         return variance
 
     @staticmethod
@@ -85,7 +86,8 @@ class EvolvingClustering:
             result = (1/num_samples)
         else:
             a = mean - x
-            result = ((1 / num_samples) + ((np.linalg.norm(a) * (1 / math.sqrt(len(a)))) ** 2 / (num_samples * var)))
+#            result = ((1 / num_samples) + ((np.linalg.norm(a) * (1 / math.sqrt(len(a)))) ** 2 / (num_samples * var)))
+            result = ((1 / num_samples) + (np.linalg.norm(a) ** 2) / (num_samples * var))
 
         return result
 
